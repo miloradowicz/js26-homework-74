3ad4399806c7137eec0d0bbe27595304c15c5c88
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-12-02 18:05:45 +0600

    express.json middleware attached

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import messagesRouter from './routers/messages';
 const app = express();
 const port = 8000;
 
+app.use(express.json());
 app.use('/messages', messagesRouter);
 
 app.listen(port, () => {
